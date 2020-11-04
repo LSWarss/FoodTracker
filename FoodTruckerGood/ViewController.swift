ba38c30488c46b3ac467a07f8bc5f6b7b065ee88
LSWarss <ls.warss98@gmail.com>
2020-11-04 15:14:02 +0100

    Initial Commit

---
+++ FoodTruckerGood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTruckerGood
+//
+//  Created by Lukasz Stachnik on 04/11/2020.
+//  Copyright © 2020 Warss. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
